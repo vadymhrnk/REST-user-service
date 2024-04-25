fcef7ed9485160ba696c223413a2df71350fd487
Vadym Hurnik <vad.com.z@gmail.com>
2024-04-25 13:02:42 +0300

    Initial commit

---
+++ src/main/java/com/example/restuserservice/RestUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restuserservice/RestUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
